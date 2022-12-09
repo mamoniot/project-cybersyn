3487746a3ca99a60d6acbfddf89507f4c4dafd1c
Monica Moniot <mamoniot@protonmail.com>
2022-12-09 16:34:20 -0500

    cheapened costs

--- cybersyn/prototypes/tech.lua
+++ cybersyn/prototypes/tech.lua
@@ -1,35 +1,25 @@
 --By Mami
-combinator_recipe = flib.copy_prototype(data.raw["recipe"]["train-stop"], COMBINATOR_NAME)
+combinator_recipe = flib.copy_prototype(data.raw["recipe"]["arithmetic-combinator"], COMBINATOR_NAME)
 combinator_recipe.ingredients = {
-	{"copper-cable", 5},
-	{"advanced-circuit", 5},
+	{"copper-cable", 20},
+	{"electronic-circuit", 10},
 }
 combinator_recipe.enabled = false
 
-cybersyn_tech = {
-	type = "technology",
-	name = "cybersyn-train-network",
-	icon = "__cybersyn__/graphics/icons/tech.png",
-	icon_size = 256,
-	--icon_mipmaps = 4,
-	prerequisites = {
-		"automated-rail-transportation",
-		"circuit-network",
-		"advanced-electronics"
-	},
-	effects = {
-		{
-			type = "unlock-recipe",
-			recipe = COMBINATOR_NAME
-		},
-	},
-	unit = {
-		ingredients = {
-			{"automation-science-pack", 1},
-			{"logistic-science-pack", 1}
-		},
-		count = 250,
-		time = 30
-	},
-	order = "c-g-c"
+
+cybersyn_tech = flib.copy_prototype(data.raw["technology"]["automated-rail-transportation"], "cybersyn-train-network")
+
+cybersyn_tech.icon = "__cybersyn__/graphics/icons/tech.png"
+cybersyn_tech.icon_size = 256
+cybersyn_tech.prerequisites = {
+	"automated-rail-transportation",
+	"circuit-network",
 }
+cybersyn_tech.effects = {
+	{
+		type = "unlock-recipe",
+		recipe = COMBINATOR_NAME
+	},
+}
+cybersyn_tech.unit.count = 3*cybersyn_tech.unit.count
+cybersyn_tech.order = "c-g-c"
