5c780f6c9981163793a8eaf7341881bc3deb3c0b
mamoniot <mamoniot@protonmail.com>
2023-05-14 13:07:49 -0400

    fixed nullius compat

--- cybersyn/data-final-fixes.lua
+++ cybersyn/data-final-fixes.lua
@@ -1,25 +1,14 @@
 flib = require("__flib__.table")
 require('scripts.constants')
 
---Credit to modo-lv for submitting the following code
 if mods["nullius"] then
+	-- Credit to modo-lv for submitting the following code
 	-- Place combinator in the same subgroup as the regular train stop
 	data.raw["recipe"][COMBINATOR_NAME].subgroup = data.raw["train-stop"]["train-stop"].subgroup
 	data.raw["item"][COMBINATOR_NAME].subgroup = data.raw["item"]["train-stop"].subgroup
 	-- Nullius makes modded technologies part of its research tree
 	-- Place combinator in the same place on the research tree as LTN
 	table.insert(data.raw.technology["nullius-broadcasting-1"].prerequisites, "cybersyn-train-network")
-end
-
-
-if mods["nullius"] then
-		--Credit to modo-lv for submitting the following code
-    -- Place combinator in the same subgroup as the regular train stop
-    data.raw["recipe"][COMBINATOR_NAME].subgroup = data.raw["train-stop"]["train-stop"].subgroup
-    data.raw["item"][COMBINATOR_NAME].subgroup = data.raw["item"]["train-stop"].subgroup
-    -- Nullius makes modded technologies part of its research tree
-    -- Place combinator in the same place on the research tree as LTN
-    table.insert(data.raw.technology["nullius-broadcasting-1"].prerequisites, "cybersyn-train-network")
 else
 	-- Reset the combinator recipe back to arithmetic combinator recipe in case a mod has changed it
 	local recipe = flib.deep_copy(data.raw["recipe"]["arithmetic-combinator"].ingredients)
